ea28d868b3bb06e893fa9489d2ffd3c29c2d2b05
Marcella <marcellatsangos@gmail.com>
2020-05-07 15:40:25 -0300

    Refatorando CSS

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,16 @@ const items = document.querySelectorAll('.item')
 const closeModal = document.querySelector('.close-modal')
 
 for (let item of items) {
-  item.addEventListener('click', () => modalOverlay.classList.add('active'))
+  item.addEventListener('click', () => {
+    const photoId = item.getAttribute('id')
+    const nomeReceita = item.querySelector('h2.nome').textContent
+    const autor = item.querySelector('p.autor').textContent
+
+    modalOverlay.classList.add('active')
+    modalOverlay.querySelector('img').src = `assets/img/${photoId}.png`
+    modalOverlay.querySelector('h1').innerText = nomeReceita
+    modalOverlay.querySelector('p').innerText = autor
+  })
 }
 
 closeModal.addEventListener('click', () => modalOverlay.classList.remove('active'))
